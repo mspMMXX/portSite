2a805fb6b21506850f9d84531522075e3bc6e84f
Markus Platter <platter.markus@gmail.com>
2025-04-21 11:57:51 +0200

    New Commit for small changes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("myButton");
+const lable = document.getElementById("inputLabel")
+let myText = "";
+
+button.addEventListener("click", () => {
+    myText = document.getElementById("input-one").ariaValueMax;
+    lable.value = myText; 
+    sdfsdf 
+})
