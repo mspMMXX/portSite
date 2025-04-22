24242956b2b35ff2ef9453e820babdfa367cf062
mspMMXX <mspMMXX@users.noreply.github.com>
2025-04-22 15:28:20 +0000

    deploy: 9509f5f3adf65e1f4a28b8f79c022c313193fbb5

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("myButton");
+const lable = document.getElementById("inputLabel")
+let myText = "";
+
+button.addEventListener("click", () => {
+    myText = document.getElementById("input-one").ariaValueMax;
+    lable.value = myText;
+})
