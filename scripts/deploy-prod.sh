13f32b36340537553fc7f150b1582c40161d2287
Markus Platter <platter.markus@gmail.com>
2025-05-22 10:54:02 +0200

    Next

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,8 +1,8 @@
 set -e
 
-docker pull $DOCKERHUB_USERNAME/portsite:$GITHUB_REF_NAME
+docker pull $DOCKERHUB_USERNAME/portsite:$IMAGE_TAG
 docker run -d \
   --name portsite-prod \
   -e NODE_ENV=production \
   -p 80:80 \
-  $DOCKERHUB_USERNAME/portsite:$GITHUB_REF_NAME
+  $DOCKERHUB_USERNAME/portsite:$IMAGE_TAG
