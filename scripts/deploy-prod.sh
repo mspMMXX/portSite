f472cc9f20ce029806532a47bcad6fe3ad3d4c3d
mspMMXX <mspMMXX@users.noreply.github.com>
2025-05-22 09:01:36 +0000

    deploy: 039412ba8c3ab3be2f24f04060313a32f70ccd9c

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,10 @@
 set -e
 
 docker pull $DOCKERHUB_USERNAME/portsite:$IMAGE_TAG
+
+docker stop portsite-prod || true
+docker rm portsite-prod || true
+
 docker run -d \
   --name portsite-prod \
   -e NODE_ENV=production \
